b7bec266e47f60291c8a9ecbcc8228e76398e1e5
Yellow_JS <89409984+YellowScript@users.noreply.github.com>
2021-08-24 07:41:54 +0700

    Create Makan.jsx
    File baru bikin

---
+++ Makan.jsx
@@ -0,0 +1,24 @@
+Menu makannan: 
+
+Baso
+Somay
+Bakmie
+Batagor
+Sayur bening
+Ikan bawal
+Sambal tomat
+Sate usus
+Sate telur puyuh
+
+
+
+Minumnya :
+
+Es tehh
+Kopi susu
+Susu sapi boyolali
+Es nutrisari
+Kopi joss areng
+Kopi aceh
+Teh kotak
+
